8aff4240641ac1e869f48181da468f447dfc56fa
shofol <jahananower@gmail.com>
2021-09-14 16:35:22 +0600

    remove unused base url

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-export const authAxios = axios.create({
-    baseURL: process.env.PUBLIC_URL,
-})
+export const authAxios = axios.create({})
 
 authAxios.interceptors.request.use((config) => {
-    config.headers.Authorization = `${localStorage.getItem('token')}`;
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
     return config;
 })
